224efc78e9f7bb4304b15efac3c81cdce7fd6672
Nwankwo Ernest <nwankwoernest2020@gmail.com>
2022-02-04 15:06:04 -0800

    tours project with react [useState and useEffect]

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
-import data from "./birthday-reminder/data";
-import List from "./birthday-reminder/List";
+import React, { useState, useEffect } from "react";
+import Loading from "./Tours-project/Loading";
+import Tours from "./Tours-project/Tours";
+const url = "https://course-api.com/react-tours-project";
 const App = () => {
-  const [people, setPeople] = useState(data);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  // fetch data
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      // console.log(tours);
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      // console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
     <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
-      </section>
+      <Tours tours={tours} />
     </main>
   );
 };
