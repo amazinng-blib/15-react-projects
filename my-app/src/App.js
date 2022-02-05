f67b2e880b94bedc559d5011d9ce43513bb5000c
Nwankwo Ernest <nwankwoernest2020@gmail.com>
2022-02-05 13:58:13 -0800

    review project with React .js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,57 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Loading from "./Tours-project/Loading";
-import Tours from "./Tours-project/Tours";
-const url = "https://course-api.com/react-tours-project";
+import React from "react";
+import Reviews from "./Reviews-project/Reviews";
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
-  // fetch data
-  const fetchTours = async () => {
-    setLoading(true);
-
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      // console.log(tours);
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      // console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className="title">
-          <h2>no tours left</h2>
-          <button className="btn" onClick={fetchTours}>
-            {" "}
-            refresh
-          </button>
-        </div>
-      </main>
-    );
-  }
   return (
     <main>
-      <Tours tours={tours} removeTour={removeTour} />
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+        </div>
+        <Reviews />
+      </section>
     </main>
   );
 };
---
+++ my-app/src/Reviews-project/README.md
@@ -0,0 +1,16 @@
+#Review project
+
+## simple Project that will diversify your knowledge in **react.js**
+
+1. ## libary used
+   _React.js_
+
+## Some methods used in the projects
+
+1. useState hookes method
+2. call back
+
+## Other amazing things to grab
+
+1. how to do proper check to avoid bugs in your code
+2. how to structure your code so that it can be readable by other developers
