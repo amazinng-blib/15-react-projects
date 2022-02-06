1b20c82fbf9759ba92bfa0d5e1f8d2e1bcbc2630
Nwankwo Ernest <nwankwoernest2020@gmail.com>
2022-02-05 16:10:35 -0800

    according project with react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
-import Reviews from "./Reviews-project/Reviews";
+import React, { useState } from "react";
+import data from "./accordion/data";
+import SingleQuestion from "./accordion/Questions";
+
 const App = () => {
+  const [questions, setQuestions] = useState(data);
   return (
     <main>
-      <section className="container">
-        <div className="title">
-          <h2>our reviews</h2>
-        </div>
-        <Reviews />
-      </section>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
     </main>
   );
 };
---
+++ my-app/src/accordion/data.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do i have to allow the use of cookes?",
+    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+  {
+    id: 2,
+    title: "How do i change my page password?",
+    info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can i use?",
+    info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    title: "When do i receive password order by letter?",
+    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+];
+
+export default questions;
